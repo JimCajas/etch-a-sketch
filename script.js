f1a18954b7b5d27381fd0841df5c24047025a409
Jim Cajas <137247877+JimCajas@users.noreply.github.com>
2023-11-17 13:55:18 +0900

    Add a pen-like hover effect

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Create a grid
 let gridBlock = 256;
 for (let i = 1; i <= gridBlock; i++) {
-    const grid = document.createElement("div");
-    grid.className = "grid-item";
+    const gridItem = document.createElement("div");
+    gridItem.className = "grid-item";
     const container = document.querySelector(".grid-container");
-    container.appendChild(grid);
+    container.appendChild(gridItem);
+
+    gridItem.addEventListener("mouseenter", () => {
+        gridItem.style.backgroundColor = "#ff4500";
+    })
 }
+
+
