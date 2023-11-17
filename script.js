51f4c2d6b0ba51d86eae5eb9759242b5cbbab63f
Jim Cajas <137247877+JimCajas@users.noreply.github.com>
2023-11-17 13:34:56 +0900

    Create 16 x 16 grid

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Create a grid
 let gridBlock = 256;
-for (let i = 0; i <= gridBlock; i++) {
+for (let i = 1; i <= gridBlock; i++) {
     const grid = document.createElement("div");
     grid.className = "grid-item";
     const container = document.querySelector(".grid-container");
