ac79ef8fd7596787e40626606afc077065ebc8ac
Jim Cajas <137247877+JimCajas@users.noreply.github.com>
2023-11-18 14:37:48 +0900

    Generate specific number of cells in the grid

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // Create a grid
-const canvasDot = 16
-let squares = canvasDot*canvasDot;
-for (let i = 1; i <= squares; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.className = "grid-item";
-    const container = document.querySelector(".grid-container");
-    container.appendChild(gridItem);
 
-    gridItem.addEventListener("mouseenter", () => {
+const gridCols = 8;
+const gridRows = 10;
+
+const grid = document.createElement("div");
+grid.className = "grid";
+const container = document.querySelector(".container");
+
+for (let i = 0; i < gridCols; i++) {
+    const column = document.createElement("div");
+    column.className = "column";
+    for (let j = 0; j < gridRows; j++) {
+        const gridItem = document.createElement("div");
+        gridItem.className = "grid-item";
+        column.appendChild(gridItem);
+
+        gridItem.addEventListener("mouseenter", () => {
         gridItem.style.backgroundColor = "#ff4500";
-    })
-}
+        })    
+    }
+    grid.appendChild(column);
+} 
+container.appendChild(grid);
 
 
