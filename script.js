0fdb9f049af6b6b9eb2672612575d07362f3bc96
Jim Cajas <137247877+JimCajas@users.noreply.github.com>
2023-11-24 17:53:25 +0900

    Ask number of squares for the sketch pad

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+
+
+
+// Ask how big the canvas should be
+let gridCols = prompt("Please enter the number of squares per side for the sketch pad. Max. 100", 64);
+if (gridCols > 100){
+    alert("The maximum number of squares is 100. Please enter again.");
+    window.location.reload();
+} 
+let gridRows = gridCols;
+
 // Create a grid
-
-const gridCols = 100;
-const gridRows = 100;
-
 const grid = document.createElement("div");
 grid.className = "grid";
 const container = document.querySelector(".container");
@@ -14,7 +21,8 @@ for (let i = 0; i < gridCols; i++) {
         const gridItem = document.createElement("div");
         gridItem.className = "grid-item";
         column.appendChild(gridItem);
-
+        
+        // Draw with a mouse movement
         gridItem.addEventListener("mouseenter", () => {
         gridItem.style.backgroundColor = "#ff4500";
         })    
@@ -23,4 +31,40 @@ for (let i = 0; i < gridCols; i++) {
 } 
 container.appendChild(grid);
 
+// Generate the sketch pad on button click
+const button = document.querySelector("button");
+button.addEventListener("click", ()=>{
 
+// Ask how big the canvas should be
+let gridCols = prompt("Please enter the number of squares per side for the sketch pad. Max. 100", 64);
+if (gridCols > 100){
+    alert("The maximum number of squares is 100. Please enter again.");
+    window.location.reload();
+} 
+let gridRows = gridCols;
+
+// Create a grid
+const grid = document.createElement("div");
+grid.className = "grid";
+const container = document.querySelector(".container");
+
+
+for (let i = 0; i < gridCols; i++) {
+    const column = document.createElement("div");
+    column.className = "column";
+    for (let j = 0; j < gridRows; j++) {
+        const gridItem = document.createElement("div");
+        gridItem.className = "grid-item";
+        column.appendChild(gridItem);
+        
+        // Draw with a mouse movement
+        gridItem.addEventListener("mouseenter", () => {
+        gridItem.style.backgroundColor = "#ff4500";
+        })    
+    }
+    grid.appendChild(column);
+} 
+container.removeChild(container.firstElementChild);
+container.appendChild(grid);
+
+})
